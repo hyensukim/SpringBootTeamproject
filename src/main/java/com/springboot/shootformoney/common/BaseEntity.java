ce3c96e699af3e34e47fe9939b95b481f4118530
hyensukim <crazymin17@naver.com>
2023-09-11 15:26:07 +0900

    공통엔티티 추가

---
+++ src/main/java/com/springboot/shootformoney/common/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.springboot.shootformoney.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+}
--- src/main/java/com/springboot/shootformoney/config/MvcConfig.java
+++ src/main/java/com/springboot/shootformoney/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.springboot.shootformoney.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaAuditing // Test 시, 사용하는 설정과 구분하기 위해 별도의 설정 클래스에 지정
 public class MvcConfig implements WebMvcConfigurer {
 
 }
