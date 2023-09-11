4aa618f0c49653f225d32e385cb647614f284b5e
unan25 <kunan25@naver.com>
2023-09-11 21:00:49 +0900

    Entity, Repository 생성

---
+++ src/main/java/com/springboot/shootformoney/admin/entity/ConfigEntity.java
@@ -0,0 +1,21 @@
+package com.springboot.shootformoney.admin.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
+
+@Entity
+@Data
+public class ConfigEntity {
+
+    @Id
+    @Column(name="code_", length = 45)
+    private String code;
+
+    @Lob
+    @Column(name = "value_")
+    private String value;
+
+}
