14527637388d1e88643a5c9db808bf74bd3650e0
Hyedokal <xcvbn5414@gmail.com>
2023-09-13 10:14:03 +0900

    DB연동 성공. PostMapping 전용 DBController 생성. 이외 메서드 수정

--- src/main/java/com/springboot/shootformoney/game/service/MatchService.java
+++ src/main/java/com/springboot/shootformoney/game/service/MatchService.java
@@ -14,7 +14,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
