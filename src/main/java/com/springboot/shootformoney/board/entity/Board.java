892cfc665e5ab2bae12c2394f1f6d5211be278d7
unan25 <kunan25@naver.com>
2023-09-14 12:35:35 +0900

    멤버 연관관계 매핑

--- src/main/java/com/springboot/shootformoney/board/entity/Board.java
+++ src/main/java/com/springboot/shootformoney/board/entity/Board.java
@@ -35,6 +35,7 @@ public class Board {
     @ColumnDefault("false")
     private boolean bIsFile; // 파일 작성 여부
 
+//    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
     @JsonIgnore // 클라이언트로부터 게시판 정보만 받고 게시물 정보는 따로 처리하려는 경우에 유용
 //    @OneToMany(mappedBy = "parent")
