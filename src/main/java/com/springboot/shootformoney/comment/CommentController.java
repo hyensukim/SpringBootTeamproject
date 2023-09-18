5932542ac0caaccb845c5d0b0a83e00400c8b648
bhPark98 <beongho7730@naver.com>
2023-09-18 14:13:05 +0900

    Comment Entity

--- src/main/java/com/springboot/shootformoney/comment/CommentController.java
+++ src/main/java/com/springboot/shootformoney/comment/CommentController.java
@@ -1,15 +1,15 @@
 package com.springboot.shootformoney.comment;
 
+import com.springboot.shootformoney.comment.CommentDto;
 import com.springboot.shootformoney.comment.CommentService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 @RequestMapping("/comments")
 public class CommentController {
 
@@ -20,14 +20,18 @@ public class CommentController {
     }
 
     @PostMapping("/{mNo}/{pNo}")
-    public ResponseEntity<Void> addComment(@PathVariable Integer mNo,
-                                           @PathVariable Integer pNo,
-                                           @RequestBody CommentDto commentDto) {
+    public String addComment(@PathVariable Integer mNo,
+                             @PathVariable Integer pNo,
+                             @RequestBody CommentDto commentDto,
+                             Model model) {
         try {
             commentService.addComment(mNo, pNo, commentDto.getContent());
-            return new ResponseEntity<>(HttpStatus.CREATED);
+            model.addAttribute("pageTitle", "Comment added successfully");
+
+            return "success";  // 'success' 라는 이름의 뷰가 있어야 합니다.
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            model.addAttribute("message", e.getMessage());
+            return "error";  // 'error' 라는 이름의 뷰가 있어야 합니다.
         }
     }
 }
--- src/main/java/com/springboot/shootformoney/file/FileController.java
+++ src/main/java/com/springboot/shootformoney/file/FileController.java
@@ -2,19 +2,20 @@ package com.springboot.shootformoney.file;
 
 import com.springboot.shootformoney.file.File;
 import com.springboot.shootformoney.file.FileService;
-
 import com.springboot.shootformoney.post.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RestController
+@Controller
 @RequestMapping("/files")
 public class FileController {
 
@@ -26,21 +27,24 @@ public class FileController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, Post post) {
+    public String uploadFile(@RequestParam("file") MultipartFile file, Post post, Model model) {
         if (file.isEmpty()) {
-            return ResponseEntity.badRequest().body("No file selected");
+            model.addAttribute("pageTitle", "No file selected");
+            return "error";
         }
 
         try {
             File uploadedFile = fileService.saveFile(file, post);
-            return ResponseEntity.ok().body("File uploaded successfully: " + uploadedFile.getFName());
+            model.addAttribute("pageTitle", "File uploaded successfully: " + uploadedFile.getFName());
+            return "success";
         } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload the file: " + e.getMessage());
+            model.addAttribute("pageTitle", "Failed to upload the file: " + e.getMessage());
+            return "error";
         }
     }
 
     @GetMapping("/download/{fileId}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable Integer fileId) {
+    public ResponseEntity<Resource> downloadFile(@PathVariable Integer fileId, Model model) {
         if (fileId == null || fileId <= 0) {
             return ResponseEntity.badRequest().body(null);
         }
@@ -53,6 +57,14 @@ public class FileController {
             }
 
             String fileName = resource.getFilename();
+
+            model.addAttribute("pageTitle", "파일 다운로드");
+
+            // download.html 뷰에 pageTitle이라는 이름으로 데이터를 전달합니다.
+
+            // 하지만 이렇게 작성할 경우 파일을 직접 다운로드 받지 못하고 페이지에 표시되는 문제가 있습니다.
+
+            // 따라서 실제 사용을 위해서는 적절한 방법을 찾아야 합니다.
 
             return ResponseEntity.ok()
                     .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
