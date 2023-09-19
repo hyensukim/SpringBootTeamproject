9cae73fb800241797ffde8e10bff8d583e93e2da
unan25 <kunan25@naver.com>
2023-09-19 15:41:49 +0900

    siteConfig 인터셉터

---
+++ src/main/java/com/springboot/shootformoney/common/interceptors/SiteConfigInterceptor.java
@@ -0,0 +1,42 @@
+package com.springboot.shootformoney.common.interceptors;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.springboot.shootformoney.admin.service.ConfigInfoService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 사이트 설정 유지
+ *
+ */
+@Component("siteConf")
+@RequiredArgsConstructor
+public class SiteConfigInterceptor implements HandlerInterceptor {
+
+    private final ConfigInfoService infoService;
+    private final HttpServletRequest request;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        /** 사이트 설정 조회 */
+        Map<String, String> siteConfigs = infoService.get("siteConfig", new TypeReference<Map<String, String>>() {});
+        request.setAttribute("siteConfig", siteConfigs);
+
+        return true;
+    }
+
+    public String get(String name) {
+        Map<String, String> siteConfig = (Map<String, String>)request.getAttribute("siteConfig");
+        String value = siteConfig == null ? "" : siteConfig.get(name);
+
+        return value;
+    }
+}
