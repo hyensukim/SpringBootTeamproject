f39dfb439705be27ce43c4a4149ef08e979e3653
unan25 <kunan25@naver.com>
2023-09-19 14:00:10 +0900

    관리자 - 게시글 프론트 병합

--- src/main/java/com/springboot/shootformoney/admin/service/postservice/PostAdminServiceImpl.java
+++ src/main/java/com/springboot/shootformoney/admin/service/postservice/PostAdminServiceImpl.java
@@ -57,4 +57,6 @@ public class PostAdminServiceImpl implements PostAdminService {
 
     }
 
+
+
 }
--- src/main/java/com/springboot/shootformoney/admin/service/postservice/PostFindService.java
+++ src/main/java/com/springboot/shootformoney/admin/service/postservice/PostFindService.java
@@ -1,10 +1,15 @@
 package com.springboot.shootformoney.admin.service.postservice;
 
+import com.springboot.shootformoney.member.entity.Member;
+import com.springboot.shootformoney.member.enum_.Grade;
+import com.springboot.shootformoney.member.enum_.Role;
 import com.springboot.shootformoney.post.Post;
 import com.springboot.shootformoney.post.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -29,6 +34,32 @@ public class PostFindService {
             return post;
         } else {
             throw new IllegalArgumentException("존재하지 않는 게시물입니다.");
+        }
+    }
+
+    // bName 조회
+    public List<Post> findPostBybName(String bName) { return postRepository.findByBoard_BName(bName); }
+
+    public List<Post> searchPosts(String category, String query) {
+        switch (category) {
+            case "pNo":
+                Post post = postRepository.findOne(Long.parseLong(query));
+                return post != null ? Arrays.asList(post) : new ArrayList<>();
+
+            case "mId":
+                return postRepository.findByMemberId(query);
+
+            case "mNickName":
+                return postRepository.findByMember_MNickName(query);
+
+            case "bName":
+                return postRepository.findByBoard_BName(query);
+
+            case "pTitle":
+                return postRepository.findByTitle(query);
+
+            default:
+                throw new IllegalArgumentException("Invalid category: " + category);
         }
     }
 
