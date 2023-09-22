08deb8badb23017f36d855f62aed9326c6e0f690
unan25 <kunan25@naver.com>
2023-09-22 14:06:11 +0900

    관리자 - 프론트 Css&Js 버전 관리 기능 구현

---
+++ src/main/java/com/springboot/shootformoney/admin/dto/AdminSearchInfo.java
@@ -0,0 +1,17 @@
+package com.springboot.shootformoney.admin.dto;
+
+import lombok.Data;
+
+@Data
+public class AdminSearchInfo {
+
+    private int page = 1;
+    private int pageSize = 10;
+
+//    private String category; // 검색 조건
+//    private String query;    // 검색 키워드 (유저 입력)
+
+//    private String sOpt; // 검색 조건
+//    private String sKey; // 검색 키워드(유저 입력)
+
+}
